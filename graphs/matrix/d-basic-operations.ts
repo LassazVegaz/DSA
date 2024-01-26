743b6b5bf0dcb977ca2e0b7ef94bf994380e2459
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-26 11:21:35 +0530

    transpose 🙃

--- graphs/matrix/d-basic-operations.ts
+++ graphs/matrix/d-basic-operations.ts
@@ -35,17 +35,47 @@ const removeEdge = (g: Graph, a: number, b: number) => {
   g[a][b] = 0;
 };
 
+const transpose = (g: Graph) => {
+  const transposedState: boolean[][] = [];
+
+  for (let i = 0; i < g.length; i++) {
+    for (let j = 0; j < g.length; j++) {
+      if (transposedState[i] !== undefined && transposedState[i][j]) continue;
+
+      if (g[i][j] === 1) {
+        if (g[j][i] === 0) {
+          g[j][i] = 1;
+          g[i][j] = 0;
+        }
+
+        if (transposedState[j] === undefined) transposedState[j] = [];
+        transposedState[j][i] = true;
+      }
+    }
+  }
+};
+
 const main = () => {
   const verticesCount = 5;
-  const g = createGraph(verticesCount);
 
+  console.log("Initializing a graph");
+  const g = createGraph(verticesCount);
+  visualize(g);
+
+  console.log("\nAdding edges: (0,2) (1,4) (4,1) (0,4) (3,1)");
   addEdge(g, 0, 2);
   addEdge(g, 1, 4);
-  addEdge(g, 2, 3);
+  addEdge(g, 4, 1);
   addEdge(g, 0, 4);
+  addEdge(g, 3, 1);
+  visualize(g);
 
-  removeEdge(g, 2, 3);
+  console.log("\nRemoving edges: (0,4)");
+  removeEdge(g, 0, 4);
+  visualize(g);
 
+  console.log("\nTransposing the graph");
+  transpose(g);
   visualize(g);
 };
 
