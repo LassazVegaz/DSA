e5a57a57f1c5ba8ce5277138d7aa0a8d8fbd64f0
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-28 20:00:36 +0530

    list graph transpose 🔗💹🙃

--- graphs/list/d-basic-operations.ts
+++ graphs/list/d-basic-operations.ts
@@ -1,6 +1,7 @@
-import LinkedList from "../../linked-list/linked-list";
+import LinkedList, { Node } from "../../linked-list/linked-list";
 import Graph from "./list-graph";
 
+//#region CREATE & VISUALIZE
 const createGraph = (vertices: number) => {
   const g: Graph = [];
 
@@ -22,9 +23,39 @@ const visualize = (g: Graph) => {
     console.log(str);
   }
 };
+//#endregion
 
 const addEdge = (g: Graph, a: number, b: number) => {
   g[a].push(b);
+};
+
+const transpose = (g: Graph) => {
+  const transposedState: number[][] = [];
+
+  for (let i = 0; i < g.length; i++) {
+    let n = g[i].head;
+    let prev: null | Node = null;
+
+    while (n !== null) {
+      if (transposedState[i]?.[n.data] > 0) {
+        transposedState[i][n.data]--;
+        prev = n;
+      } else {
+        g[n.data].enqueue(i);
+
+        if (prev === null) {
+          g[i].head = n.next;
+        } else {
+          prev.next = n.next;
+        }
+
+        transposedState[n.data] ??= [];
+        transposedState[n.data][i] ??= 0;
+        transposedState[n.data][i]++;
+      }
+      n = n.next;
+    }
+  }
 };
 
 const main = () => {
@@ -38,6 +69,10 @@ const main = () => {
   addEdge(g, 3, 4);
   addEdge(g, 1, 0);
   console.log("Edges added to graph: (0,1) (4,1) (4,2) (3,4) (1,0)");
+  visualize(g);
+
+  transpose(g);
+  console.log("Graph transposed");
   visualize(g);
 };
 
