9469f944cc703c56ac132e7a3cdbf9bffb6c604f
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-28 18:55:59 +0530

    linked list basics 🔗

---
+++ linked-list/linked-list.ts
@@ -0,0 +1,28 @@
+class Node {
+  next: Node | null = null;
+
+  constructor(public data: number) {}
+}
+
+export default class LinkedList {
+  head: Node | null = null;
+
+  getTail() {
+    if (!this.head) return null;
+
+    let tail = this.head;
+
+    while (tail.next !== null) tail = tail.next;
+
+    return tail;
+  }
+
+  push(value: number) {
+    const node = new Node(value);
+
+    if (!this.head) this.head = node;
+    else {
+      this.getTail()!.next = node;
+    }
+  }
+}
