5216346d4008544ae140bdc5c7489af551a5a553
LassazVegaz <lasindunuwanga@gmail.com>
2024-02-16 11:22:44 +0530

    dfs (doy friends suck) ✌️🌲 - simplified

--- tree/binary-tree/dfs.test.ts
+++ tree/binary-tree/dfs.test.ts
@@ -1,18 +1,9 @@
+import { createTestTree } from "./basic-operations";
 import Tree, { BinaryTreeNode as Node } from "./binary-tree";
 import dfs from "./dfs";
 
 const main = () => {
-  const t = new Tree();
-  t.root = new Node(1);
-  t.root.left = new Node(2);
-  t.root.right = new Node(3);
-  t.root.left.left = new Node(4);
-  t.root.left.right = new Node(5);
-  t.root.right.left = new Node(6);
-  t.root.right.right = new Node(7);
-  t.root.left.left.right = new Node(8);
-  t.root.right.right.left = new Node(9);
-
+  const t = createTestTree();
   console.log("DFS on binary tree");
   dfs(t);
 };
