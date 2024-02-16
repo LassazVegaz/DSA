10b0c095c4fee384b6febf2c6aa7968043e6c46f
LassazVegaz <lasindunuwanga@gmail.com>
2024-02-16 11:09:28 +0530

    queue 👆👇

---
+++ queue/queue.ts
@@ -0,0 +1,21 @@
+export default class Queue<T> {
+  private arr: T[] = [];
+
+  enqueue(data: T) {
+    this.arr.push(data);
+  }
+
+  dequeue(): T {
+    if (this.arr.length === 0) throw new Error("Queue is empty");
+    return this.arr.shift()!;
+  }
+
+  isEmpty() {
+    return this.arr.length === 0;
+  }
+
+  peek() {
+    if (this.arr.length === 0) throw new Error("Queue is empty");
+    return this.arr[0];
+  }
+}
